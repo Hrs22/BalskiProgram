166820fa281db0484c66586a3862e3c3ea0a8388
Radostin2 <alah51@abv.bg>
2023-01-08 22:06:30 +0200

    First Commit

--- ChristmasProgram/View/RegisterView.Designer.cs
+++ ChristmasProgram/View/RegisterView.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace ChristmasProgram
+﻿namespace ChristmasProgram.View
 {
-    partial class Form1
+    partial class RegisterView
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // RegisterView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(436, 672);
+            this.Name = "RegisterView";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RegisterView";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
     }
 }
-
