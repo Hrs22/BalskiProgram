21aff767630490fdc824d7f9836294daa47c7cb5
Radostin2 <alah51@abv.bg>
2023-01-23 19:03:34 +0200

    Edited the login view

--- ChristmasProgram/View/LoginView.cs
+++ ChristmasProgram/View/LoginView.cs
@@ -16,5 +16,43 @@ namespace ChristmasProgram.View
         {
             InitializeComponent();
         }
+        private void txtBoxEmail_MouseClick(object sender, MouseEventArgs e)
+        {
+            txtBoxEmail.Clear();
+        }
+
+        private void txtBoxPassword_MouseClick(object sender, MouseEventArgs e)
+        {
+            txtBoxPassword.Clear();
+        }
+
+        private void btnLogIn_Click(object sender, EventArgs e)
+        {
+            if (true)
+            {
+                MessageBox.Show("Succsesfully logged!");
+                MainView mv = new MainView();
+                this.Hide();
+                mv.ShowDialog();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Something went wrong!");
+            }
+        }
+
+        private void btnCreateAcc_Click(object sender, EventArgs e)
+        {
+            RegisterView rv = new RegisterView();
+            this.Hide();
+            rv.ShowDialog();
+            this.Close();
+        }
+
+        private void btnCantLog_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Sori motori nikoi ne mi plashta da pomagam");
+        }
     }
 }
