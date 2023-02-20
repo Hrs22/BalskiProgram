3523142a6e9ada6e6a54e3c5a2bdfbcd4fbbcd05
Radostin2 <alah51@abv.bg>
2023-02-20 22:08:25 +0200

    update 4

--- ChristmasProgram/View/MainView.Designer.cs
+++ ChristmasProgram/View/MainView.Designer.cs
@@ -28,13 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainView));
+            this.txtBoxEmail = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
+            // 
+            // txtBoxEmail
+            // 
+            this.txtBoxEmail.AccessibleName = "";
+            this.txtBoxEmail.BackColor = System.Drawing.SystemColors.Info;
+            this.txtBoxEmail.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.txtBoxEmail.Font = new System.Drawing.Font("MV Boli", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxEmail.Location = new System.Drawing.Point(238, 160);
+            this.txtBoxEmail.Name = "txtBoxEmail";
+            this.txtBoxEmail.Size = new System.Drawing.Size(301, 30);
+            this.txtBoxEmail.TabIndex = 1;
+            this.txtBoxEmail.Text = "    Still work in progress :D";
             // 
             // MainView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Indigo;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtBoxEmail);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainView";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
@@ -43,6 +61,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox txtBoxEmail;
     }
 }
 
